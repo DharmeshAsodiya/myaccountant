a3ce9682befe898c285360a264bef386b1f50c5f
dharmesh.asodiya <dharmesh.asodiya@gmail.com>
2020-06-26 22:19:18 +0530

    changed static url in settings

--- myaccountant/settings.py
+++ myaccountant/settings.py
@@ -134,14 +134,15 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
+
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+# STATICFILES_FINDERS = [
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# ]
 
