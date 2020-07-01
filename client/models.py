7b28c7f31af5ffd524bc4f096cdb1d6cb9310d11
dharmesh.asodiya <dharmesh.asodiya@gmail.com>
2020-07-02 00:40:17 +0530

    invoice change

--- client/models.py
+++ client/models.py
@@ -5,8 +5,8 @@ from django.utils.translation import ugettext_lazy as _
 
 class Shop(BaseModel):
     name = models.CharField(max_length=200)
-    contact_no = models.CharField(_("Mobile Number"), max_length=30)
-    email = models.EmailField(max_length=100)
+    contact_no = models.CharField(_("Mobile Number"), max_length=30, null=True, blank=True)
+    email = models.EmailField(max_length=100, default="", null=True, blank=True)
     address = models.TextField(max_length=1000)
     beat = models.CharField(max_length=200)
 
--- order/services/ordercreator.py
+++ order/services/ordercreator.py
@@ -15,19 +15,28 @@ class OrderCreator(object):
         self.order = None
         self.shop = None
         self.product_map = None
+        self.tax_details = None
+        self.tax_total = Decimal(0)
         self._build_order_details(shop_id, product_list, price_list, quantity_list)
 
     def _build_order_details(self, shop_id, product_list, price_list, quantity_list):
         self.shop = Shop.objects.get(id=shop_id)
         self.product_map = Product.objects.in_bulk(id_list=product_list)
         print(self.product_map)
+        self.tax_details = {}
         for idx in range(len(product_list)):
             p_id = product_list[idx]
             product = self.product_map.get(int(p_id))
             quantity = Decimal(quantity_list[idx])
             price = Decimal(price_list[idx])
-            product_total = product.tax_value/100 * (quantity * price) + (quantity * price)
+            tax_value = (product.tax_value / 100 * (quantity * price)).quantize(Decimal(".01"), rounding=ROUND_UP)
+            product_total = quantity * price
             product_total = product_total.quantize(Decimal(".01"), rounding=ROUND_UP)
+            tax = self.tax_details.get(product.tax_value)
+            if tax:
+                self.tax_details.update({product.tax_value/2: (tax + tax_value)/2})
+            else:
+                self.tax_details.update({product.tax_value/2: tax_value/2})
             item = {
                 "product_id": product.id,
                 "product_desc": product.name,
@@ -41,17 +50,22 @@ class OrderCreator(object):
             }
             self.item_details.append(item)
             self.order_total += product_total
+            self.tax_total += tax_value
 
     def get_invoice_data(self):
         inf = inflect.engine()
+        print(self.tax_details)
+        final_total = self.order_total + sum(self.tax_details.values()) * 2
         return {"invoice_no": self.order.id if self.order else "NA",
                 "invoice_date": datetime.datetime.strftime(datetime.datetime.now(), "%d/%m/%Y"),
                 "shop_name": self.shop.name,
                 "shop_address": self.shop.address,
                 "item_details": self.item_details,
                 "sub_total": self.order_total,
+                "final_total": final_total,
+                "tax_details": self.tax_details,
                 "total_in_words":
-                    inf.number_to_words(self.order_total).split("point")[0].capitalize()
+                    inf.number_to_words(final_total).split("point")[0].capitalize()
                 }
 
     def create_order(self):
