c79e11093a28f491c84f68dc1314ee493437b7f1
dharmesh.asodiya <dharmesh.asodiya@gmail.com>
2020-12-14 00:05:09 +0530

    utility fix

--- order/services/ordercreator.py
+++ order/services/ordercreator.py
@@ -107,3 +107,27 @@ class OrderCreator(object):
         #                                       outstanding_amount=self.total_payable,
         #                                       invoice_details=json.dumps(details))
         return self.order.id
+
+def get_invoice_details(start_date, end_date):
+    import json
+    orders = Order.objects.filter(created_on__gte=start_date,
+                                  created_on__lt=end_date).order_by("id")
+    print("INV NO, DATE, GST NO, TAXABLE VALUE, SGST 2.5%, CGST 2.5%, SGST 6%, CGST 6%, TOTAL")
+    for o in orders:
+        data = json.loads(o.invoice_details)
+        tax = 0
+        value = 0
+        tax_5 = data["tax_details"].get("2.5", [0])[0]
+        tax_12 = data["tax_details"].get("6.0", [0])[0]
+        for key, val in data["tax_details"].items():
+            tax += float(key)
+            value += float(val[1])
+        print(
+            f"{data['invoice_no']}, {data['invoice_date']}, {data.get('shop_gst', '')}, {value}, {tax_5}, "
+            f"{tax_5}, {tax_12}, {tax_12}, {data['sub_total']}")
+
+get_invoice_details("2020-07-01", "2020-08-01")
+
+get_invoice_details("2020-08-01", "2020-09-01")
+
+get_invoice_details("2020-09-01", "2020-10-01")
